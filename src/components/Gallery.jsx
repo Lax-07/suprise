42821e52c5a9dc246178c24c376e7fe8db735361
Lax-07 <lakshitagarwal748@gmail.com>
2026-01-06 21:00:49 +0530

    times

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -139,7 +139,7 @@ function Gallery({ isActive }) {
 
   return (
     <section className="gallery">
-      <h2>📸 Our Beautiful Memories</h2>
+      <h2>📸 The Mainss</h2>
       <div className="photos">
         {photos.map((photo, index) => (
           <img
