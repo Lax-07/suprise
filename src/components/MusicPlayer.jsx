ff4b6bb766c5e22b60981143d9650a3be37eee45
Lax-07 <lakshitagarwal748@gmail.com>
2026-01-09 19:08:26 +0530

    now will work

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -74,7 +74,7 @@ const MusicPlayer = forwardRef((props, ref) => {
   return (
     <>
       <audio ref={audioRef} loop preload="auto">
-        src = "/music.mp3"
+      <source src="/music.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
       <button
